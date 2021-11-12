7026bea6dba8e29680daefb79ed5533a807636b2
Natasha Maruska Navarro <nmfnavarro@gmail.com>
2021-11-12 08:27:05 +0800

    add plugin boilerplate

---
+++ user/scripts/scripts.js
@@ -0,0 +1,48 @@
+(function () {
+  /* globals $ */
+  var scriptSrc = document.currentScript.src;
+  var re = /([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/i;
+  var packageId = re.exec(scriptSrc.toLowerCase())[1];
+  var packagePath = scriptSrc.replace("/scripts/scripts.js", "").trim();
+  var customFieldPrefix = packageId.replace(/-/g, "");
+  const HOST = window.location.host;
+  var hostname = window.location.hostname;
+  var urls = window.location.href.toLowerCase();
+  var userId = $("#userGuid").val();
+
+  
+  function waitForElement(elementPath, callBack) {
+    window.setTimeout(function () {
+      if ($(elementPath).length) {
+        callBack(elementPath, $(elementPath));
+      } else {
+        waitForElement(elementPath, callBack);
+      }
+    }, 500);
+  }
+
+  function getMarketplaceCustomFields(callback) {
+    var apiUrl = "/api/v2/marketplaces";
+    $.ajax({
+      url: apiUrl,
+      method: "GET",
+      contentType: "application/json",
+      success: function (result) {
+        if (result) {
+          callback(result.CustomFields);
+        }
+      },
+    });
+  }
+
+
+  $(document).ready(function () {
+    getMarketplaceCustomFields(function (result) {
+      $.each(result, function (index, cf) {
+       
+      });
+    });
+
+  
+  });
+})();

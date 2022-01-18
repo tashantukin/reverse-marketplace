b78903e81f88b7c4d58eb48276138087651599ba
Natasha Maruska Navarro <nmfnavarro@gmail.com>
2022-01-19 05:46:36 +0800

    fix edit delete buyer fields

--- admin/delete_customfield.php
+++ admin/delete_customfield.php
@@ -4,7 +4,8 @@ include 'admin_token.php';
 $contentBodyJson = file_get_contents('php://input');
 $content = json_decode($contentBodyJson, true);
 
-$custom_code = $content['code'];
+
+$field_id = $content['code'];
 
 $baseUrl = getMarketplaceBaseUrl();
 $admin_token = getAdminToken();
@@ -14,6 +15,21 @@ $url = $baseUrl . '/api/v2/users/';
 $result = callAPI("GET", $admin_token['access_token'], $url, false);
 $userId = $result['ID'];
 
-$url = $baseUrl . '/api/v2/admins/' . $userId . '/custom-field-definitions/' . $custom_code;
-$result = callAPI("DELETE", $admin_token['access_token'], $url, false);
-echo json_encode(['result' => $result]);
+
+    //edit
+    // if ($refTable == 'Users') {
+
+    //     $form_details = array('name' => $custom_name, 'type_of_field' => $type, 'classification' => $classification, 'text' => $custom_text, 'placeholder' => $placeholder, 'values' => $type !== 'textfield' || $type !== 'number' || $type !== 'search'? $options : null );
+    //     $url =  $baseUrl . '/api/v2/plugins/'. getPackageID() .'/custom-tables/freelancer_form/rows';
+    //     $result =  callAPI("POST",$admin_token['access_token'], $url, $form_details);
+        
+    // }else {
+        //$form_details = array('name' => $custom_name, 'type_of_field' => $type, 'classification' => $classification, 'text' => $custom_text, 'placeholder' => $placeholder,  'values' => $type !== 'textfield' || $type !== 'number' || $type !== 'search' ? $options : null );
+        $url =  $baseUrl . '/api/v2/plugins/'. getPackageID() .'/custom-tables/job_form/rows/' . $field_id;;
+        $result =  callAPI("DELETE", $admin_token['access_token'], $url);
+    // }
+
+// }
+
+
+
